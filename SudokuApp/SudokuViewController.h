67bf39e1a5ace0c14f2d69bcaef7c0a42ce30eec
Daniel Colligan [el16dc] <el16dc@eentpc99.leeds.ac.uk>
2017-11-20 16:01:54 +0000

    Started Code Segue between first 2 pages

--- SudokuApp/SudokuViewController.h
+++ SudokuApp/SudokuViewController.h
@@ -4,12 +4,13 @@
 //
 //  Created by Daniel Colligan [el16dc] on 20/11/2017.
 //  Copyright © 2017 Daniel Colligan [el16dc]. All rights reserved.
-//
+// edit to commit
 
 #import <UIKit/UIKit.h>
 
 @interface SudokuViewController : UIViewController
 
 @property (weak, nonatomic) IBOutlet UILabel *SudokuGameScreen;
+
 
 @end
